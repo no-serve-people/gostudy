9a6de4871c029dcf164a7dab14914eb74f783576
leafmirror <ixingjue@qq.com>
2021-04-29 19:02:49 +0800

    WaitGroup

---
+++ goroutine/test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func Main() {
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(10 * time.Second)
+		}()
+
+	}
+	wg.Wait() // 等待在此，等所有go func里都执行了Done()才会退出
+}
