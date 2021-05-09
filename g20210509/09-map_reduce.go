798eba11b35d3b46cd7d1a229a21c9ad5609abac
leafmirror <ixingjue@qq.com>
2021-05-09 09:29:16 +0800

    Go 函数式编程篇（六）：引入 Map-Reduce-Filter 模式处理集合元素

---
+++ g20210509/09-map_reduce.go
@@ -0,0 +1,37 @@
+package main
+
+// https://geekr.dev/posts/go-func-map-reduce-filter-mode
+// Go 函数式编程篇（六）：引入 Map-Reduce-Filter 模式处理集合元素
+// 从处理集合元素聊起
+import (
+	"fmt"
+	"strconv"
+)
+
+func ageSum(users []map[string]string) int {
+	var sum int
+	for _, user := range users {
+		num, _ := strconv.Atoi(user["age"])
+		sum += num
+	}
+	return sum
+}
+
+func main() {
+	var users = []map[string]string{
+		{
+			"name": "张三",
+			"age":  "18",
+		},
+		{
+			"name": "李四",
+			"age":  "22",
+		},
+		{
+			"name": "王五",
+			"age":  "20",
+		},
+	}
+
+	fmt.Printf("用户年龄累加结果是:%d\n", ageSum(users))
+}
