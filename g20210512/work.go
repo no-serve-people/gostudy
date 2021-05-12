e5eb35a642bd2f91ccc09612cd14de9307156d9e
leafmirror <ixingjue@qq.com>
2021-05-12 13:14:25 +0800

    常见的并发模式实现（三）：通过无缓冲通道创建协程池

---
+++ g20210512/work.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"gostudy/g20210512/worker"
+	"log"
+	"sync"
+	"time"
+)
+
+var langs = []string{
+	"Golang",
+	"PHP",
+	"JavaScript",
+	"Python",
+	"Java",
+}
+
+type langPrinter struct {
+	lang string
+}
+
+func (m *langPrinter) Task() {
+	log.Println(m.lang)
+	time.Sleep(time.Second)
+}
+
+func main() {
+	p := worker.New(2)
+	var wg sync.WaitGroup
+	wg.Add(5 * len(langs))
+
+	for i := 0; i < 5; i++ {
+		for _, lang := range langs {
+			lp := langPrinter{lang}
+			go func() {
+				p.Run(&lp)
+				wg.Done()
+			}()
+		}
+	}
+	wg.Wait()
+	p.Shutdown()
+}
