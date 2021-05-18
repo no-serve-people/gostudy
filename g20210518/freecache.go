5e2e5ea0ebb5e30c99149a5fabda86044944e9ce
leafmirror <ixingjue@qq.com>
2021-05-18 15:14:25 +0800

    Go缓冲系列之-free-cache

---
+++ g20210518/freecache.go
@@ -0,0 +1,34 @@
+package g20210518
+
+import (
+	"fmt"
+	"github.com/coocood/freecache"
+	"runtime/debug"
+)
+// Go缓冲系列之-free-cache
+// https://mp.weixin.qq.com/s/SwGTdFe9AgHPGqkDbZoUjQ
+func main() {
+	// 缓存大小，100M
+	cacheSize := 100 * 1024 * 1024
+	cache := freecache.NewCache(cacheSize)
+	debug.SetGCPercent(20)
+	key := []byte("abc")
+	val := []byte("def")
+
+	expire := 60 // expire in 60 seconds
+	// 设置 key
+	cache.Set(key, val, expire)
+	got, err := cache.Get(key)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("$s\n", got)
+	}
+	fmt.Println("entry count ", cache.EntryCount())
+	affected := cache.Del(key)
+
+	fmt.Println("deleted key ", affected)
+	fmt.Println("entry count ", cache.EntryCount())
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gostudy
 
 go 1.16
+
+require github.com/coocood/freecache v1.1.1 // indirect
