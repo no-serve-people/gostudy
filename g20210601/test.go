0ad7b8f81cd72e088fce3f9aa7410858bd3d6077
leafmirror <ixingjue@qq.com>
2021-06-01 15:32:28 +0800

    源码剖析sync.WaitGroup(文末思考题你能解释一下吗?)

--- g20210601/test.go
+++ g20210601/test.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 )
 
+// https://mp.weixin.qq.com/s?__biz=MzIzMDU0MTA3Nw==&mid=2247484551&idx=1&sn=aff008de8b6c089d7daf9c963ce8b60a&scene=21#wechat_redirect
+// 源码剖析sync.WaitGroup(文末思考题你能解释一下吗?)
 func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(100)
