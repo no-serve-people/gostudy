29081173ae017fa2675b11ccb8bb68e3f2ddd3e2
leafmirror <ixingjue@qq.com>
2021-06-04 12:52:19 +0800

    i/o timeout ， 希望你不要踩到这个net/http包的坑

---
+++ g20210604/io/iotest01.go
@@ -0,0 +1,66 @@
+package main
+// https://mp.weixin.qq.com/s/Isjv1fTYd0m8yrjONINsWw
+// i/o timeout ， 希望你不要踩到这个net/http包的坑
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
+)
+
+var tr *http.Transport
+
+func init() {
+	tr = &http.Transport{
+		MaxIdleConns: 100,
+		Dial: func(netw, addr string) (net.Conn, error) {
+			conn, err := net.DialTimeout(netw, addr, time.Second*2)
+			//设置建立连接超时
+			if err != nil {
+				return nil, err
+			}
+			err = conn.SetDeadline(time.Now().Add(time.Second * 3)) //设置发送接受数据超时
+			if err != nil {
+				return nil, err
+			}
+			return conn, nil
+		},
+	}
+}
+
+func Get(url string) ([]byte, error) {
+	m := make(map[string]interface{})
+	data, err := json.Marshal(m)
+
+	if err != nil {
+		return nil, err
+	}
+	body := bytes.NewReader(data)
+	req, _ := http.NewRequest("Get", url, body)
+	req.Header.Add("content-type", "application/json")
+	client := &http.Client{Transport: tr}
+	res, err := client.Do(req)
+	if res != nil {
+		defer res.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return resBody, nil
+}
+func main() {
+	for {
+		_, err := Get("http://www.baidu.com/")
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+}
