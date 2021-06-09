375f23ddded1add3549f8bda73bdc2e051825a28
leafmirror <ixingjue@qq.com>
2021-06-09 15:14:15 +0800

    range benchmark

---
+++ g20210609/range_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+type Item struct {
+	Id   int64
+	Name string
+}
+
+func BenchmarkRangeSpeed01(b *testing.B) {
+	var items [1024]Item
+
+	for i := 0; i < b.N; i++ {
+		var tmp int64
+		for _, item := range items {
+			tmp = item.Id
+		}
+		//fmt.Println(tmp)
+		_ = tmp
+	}
+}
+
+func BenchmarkRangeSpeed02(b *testing.B) {
+	var items [1024]Item
+
+	for i := 0; i < b.N; i++ {
+		var tmp int64
+		for k, _ := range items {
+			item := items[k]
+			tmp = item.Id
+		}
+		//fmt.Println(tmp)
+		_ = tmp
+	}
+}
