8322273e7577e7153eae881dd36339055d2dc26e
leafmirror <ixingjue@qq.com>
2021-08-05 11:30:44 +0800

    Go语言的并发与WorkerPool - 第二部分

---
+++ g20210805/workpool/task.go
@@ -0,0 +1,22 @@
+package workpool
+
+import "fmt"
+
+type Task struct {
+	Err  error
+	Data interface{}
+	f    func(interface{}) error
+}
+
+func NewTask(f func(interface{}) error, data interface{}) *Task {
+	return &Task{
+		Data: data,
+		f:    f,
+	}
+}
+// Go语言的并发与WorkerPool - 第二部分
+// https://mp.weixin.qq.com/s/u3h8mfNja_SAnFP4vawCGg
+func process(workerID int, task *Task) {
+	fmt.Printf("Worker %d processes task %v\n", workerID, task.Data)
+	task.Err = task.f(task.Data)
+}
---
+++ g20210805/workpool/worker.go
@@ -0,0 +1,59 @@
+package workpool
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Worker handles all the work
+type Worker struct {
+	ID       int
+	taskChan chan *Task
+	quit     chan bool
+}
+
+// NewWorker returns new instance of worker
+func NewWorker(channel chan *Task, ID int) *Worker {
+	return &Worker{
+		ID:       ID,
+		taskChan: channel,
+		quit:     make(chan bool),
+	}
+}
+
+// Start starts the worker
+func (wr *Worker) Start(wg *sync.WaitGroup) {
+	fmt.Printf("Starting worker %d\n", wr.ID)
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for task := range wr.taskChan {
+			process(wr.ID, task)
+		}
+	}()
+}
+
+// StartBackground starts the worker in background waiting
+func (wr *Worker) StartBackground() {
+	fmt.Printf("Starting worker %d\n", wr.ID)
+
+	for {
+		select {
+		case task := <-wr.taskChan:
+			process(wr.ID, task)
+		case <-wr.quit:
+			return
+		}
+	}
+}
+// Go语言的并发与WorkerPool - 第二部分
+// https://mp.weixin.qq.com/s/u3h8mfNja_SAnFP4vawCGg
+// Stop quits the worker
+func (wr *Worker) Stop() {
+	fmt.Printf("Closing worker %d\n", wr.ID)
+	go func() {
+		wr.quit <- true
+	}()
+}
