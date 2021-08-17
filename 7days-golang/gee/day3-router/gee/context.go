44186d295f36e2159c21b511b7ad4feaba8e95e9
leafmirror <ixingjue@qq.com>
2021-08-17 20:58:50 +0800

    test

---
+++ 7days-golang/gee/day3-router/gee/context.go
@@ -0,0 +1,20 @@
+package gee
+
+import "net/http"
+
+type Context struct {
+	// origin objects
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// request info
+	Path   string
+	Method string
+	Params map[string]string
+	// response info
+	StatusCode int
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
