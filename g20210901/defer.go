5f19bb4623ae01bb5fbdf11e0b3187e6dd2f2591
leafmirror <ixingjue@qq.com>
2021-09-01 19:21:07 +0800

    defer

---
+++ g20210901/defer.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(do())
+}
+func do() string {
+	defer fmt.Println("do defer")
+	fmt.Println("do something")
+	//panic("do panic")
+
+	return "do return"
+}
