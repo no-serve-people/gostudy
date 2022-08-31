f92730964af0394f9e0cd66d269122ae196c5e6f
no-serve-people <ixingjue@qq.com>
2022-08-31 11:13:56 +0800

    最长回文子串

---
+++ codetop/longest-palindromic-substring.go
@@ -0,0 +1,32 @@
+package codetop
+
+var srange = [2]int{}
+
+func longestPalindrome(s string) string {
+	n := len(s)
+	if n < 2 {
+		return s
+	}
+	for i := 1; i < n; i++ {
+		helper(s, n, i, i+1)
+		helper(s, n, i, i)
+	}
+
+	return s[srange[0]:srange[1]]
+}
+
+func helper(s string, n, start, end int) {
+	for start >= 0 && end <= n-1 {
+		if s[start] == s[end] {
+			start--
+			end++
+		} else {
+			break
+		}
+	}
+
+	if end-(start+1) > srange[1]-srange[0] {
+		srange[0] = start + 1
+		srange[1] = end
+	}
+}
---
+++ codetop/longest-palindromic-substring_test.go
@@ -0,0 +1,25 @@
+package codetop
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1", args{"babad"}, "bab"},
+		{"2", args{"cbb"}, "bb"},
+		{"3", args{"cbbd"}, "bb"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
