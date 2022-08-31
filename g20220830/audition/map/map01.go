ba848230be5809b462a0f6d08798366662fe8d73
no-serve-people <ixingjue@qq.com>
2022-08-31 14:46:20 +0800

    map学习

---
+++ g20220830/audition/map/map01.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//m := make(map[string]int)
+	//
+	//fmt.Println(&m["qcrao"])
+
+	var m map[string]int
+	var n map[string]int
+
+	fmt.Println(m == nil)
+	fmt.Println(n == nil)
+	//fmt.Println(m==n)
+}
---
+++ g20220830/audition/map/map02.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	ageMap := make(map[string]int)
+	ageMap["qcrao"] = 18
+	// 不带comma的用法
+	age1 := ageMap["stefno"]
+	fmt.Println(age1)
+	// 带comma的用法
+	age2, ok := ageMap["stefno"]
+	fmt.Println(age2, ok)
+}
