f42fba756334e85308f9bf924889ab0a45f8c0fe
no-serve-people <ixingjue@qq.com>
2022-09-01 10:35:27 +0800

    无重复字符的最长子串

---
+++ codetop/longest-substring-without-repeating-characters.go
@@ -0,0 +1,30 @@
+package codetop
+
+// 输入: s = "abcabcbb"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+func lengthOfLongestSubstring(s string) int {
+	n := len(s)
+	if n < 2 {
+		return n
+	}
+
+	left, right, maxlength := 0, 0, 1
+
+	window := make(map[byte]bool)
+	for right < n {
+		rightChar := s[right]
+		for window[rightChar] {
+			delete(window, s[left])
+			left++
+		}
+
+		if right-left+1 > maxlength {
+			maxlength = right - left + 1
+		}
+		window[rightChar] = true
+		right++
+	}
+
+	return maxlength
+}
