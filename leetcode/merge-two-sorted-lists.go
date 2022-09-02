49234b4982d0b00d238f025068668c27ae29064d
no-serve-people <ixingjue@qq.com>
2022-09-02 23:22:26 +0800

    合并两个有序链表递归

--- leetcode/merge-two-sorted-lists.go
+++ leetcode/merge-two-sorted-lists.go
@@ -1,45 +1,15 @@
 package leetcode
 
-import "sort"
-
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-
-	list := make([]int, 0)
-
-	h1 := list1
-	h2 := list2
-	for h1 != nil {
-		list = append(list, h1.Val)
-		h1 = h1.Next
+	if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	} else if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
 	}
-
-	for h2 != nil {
-		list = append(list, h2.Val)
-		h2 = h2.Next
-	}
-
-	sort.SliceStable(list, func(i, j int) bool {
-		return list[i] < list[j]
-	})
-
-	return newListNode(list)
-}
-
-// newListNode 生成一个链表
-func newListNode(arr []int) *ListNode {
-	var head ListNode
-	var pre ListNode
-	for _, num := range arr {
-		node := ListNode{Val: num, Next: nil}
-		if head.Next == nil {
-			head.Next = &node
-		}
-		if pre.Next == nil {
-			pre.Next = &node
-		}else {
-			pre.Next.Next = &node
-			pre = *pre.Next
-		}
-	}
-	return head.Next
 }
