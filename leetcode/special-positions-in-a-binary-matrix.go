2cb22c5176c874066c487f31fab34e609b9777e7
no-serve-people <ixingjue@qq.com>
2022-09-04 09:50:45 +0800

    二进制矩阵中的特殊位置

---
+++ leetcode/special-positions-in-a-binary-matrix.go
@@ -0,0 +1,24 @@
+package leetcode
+
+// 模拟法
+func numSpecial(mat [][]int) (ans int) {
+	rows := make([]int, len(mat))
+	cols := make([]int, len(mat[0]))
+
+	for i, row := range mat {
+		for j, x := range row {
+			rows[i] += x
+			cols[j] += x
+		}
+	}
+
+	for i, row := range mat {
+		for j, x := range row {
+			if x == 1 && rows[i] == 1 && cols[j] == 1 {
+				ans++
+			}
+		}
+	}
+
+	return
+}
---
+++ leetcode/special-positions-in-a-binary-matrix_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "testing"
+
+func Test_numSpecial(t *testing.T) {
+	type args struct {
+		mat [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{[][]int{{1, 0, 0}, {0, 0, 1}, {1, 0, 0}}}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numSpecial(tt.args.mat); got != tt.want {
+				t.Errorf("numSpecial() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
