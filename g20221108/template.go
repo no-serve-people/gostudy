908b0eaa68e71d59a94dc9547a61f5948eb3d59f
no-serve-people <ixingjue@qq.com>
2022-11-08 22:12:38 +0800

    test template

--- g20221108/template.go
+++ g20221108/template.go
@@ -3,29 +3,66 @@ package g20221108
 import (
 	"log"
 	"os"
-	"strings"
 	"text/template"
 )
 
 type TestData struct {
 	Name       string `json:"name"`
 	Attributes map[string]interface{}
+	Address    *struct {
+		Street string
+	}
 }
 
 func testTemplate() {
-	var Template = `{{ range $key, $value := .Attributes }}
-	{{ $.Name}} {{ $key }}: {{ $value }}{{- end }}`
-	tmpl, err := template.New("test").Parse(strings.TrimSpace(Template))
+	var Template = `apiVersion: apps/v1
+kind: {{.Name}}
+metadata:
+  name: nginx-deployment
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  replicas: 2 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+        - name: nginx
+          image: nginx:1.14.2
+          ports:
+            - containerPort: 80
+
+
+       {{ range $key, $value := .Attributes }}
+        - name: {{ $key }}
+          value: {{ $value }}
+      {{ end }}
+
+  {{if .Address}}
+  Apt is {{.Address.Street}}.
+  {{else}}
+  Apt is NOT set.
+  {{end}}
+`
+	tmpl, err := template.New("test").Parse(Template)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	data := TestData{
-		"hello",
-		map[string]interface{}{
+		Name: "hello",
+		Attributes: map[string]interface{}{
 			"key":   "hello",
 			"value": "json",
 		},
+		//Address: struct {
+		//	Street string
+		//}{
+		//	Street: "牛逼",
+		//},
 	}
 	err = tmpl.Execute(os.Stdout, data)
 	if err != nil {
